bb32fd7638afcd401e0ff5289c32db481b52e83a
Thales Fernandes <ThalesFDFernandes@gmai.com>
2023-05-31 18:46:52 +0200

    new node

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { sveltekit } from '@sveltejs/kit/vite'
 
-export default defineConfig({
-	plugins: [sveltekit()]
-});
+/** @type {import('vite').UserConfig} */
+const config = {
+  plugins: [sveltekit()],
+}
+
+export default config
